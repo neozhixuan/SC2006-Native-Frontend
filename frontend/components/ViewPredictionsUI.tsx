ce20d8050a67300ae1b4e847bebb8e05b5291203
Zhi <neozhixuan@gmail.com>
2023-11-03 02:54:26 +0800

    Feature - Placeholder Graphics

--- frontend/components/ViewPredictionsUI.tsx
+++ frontend/components/ViewPredictionsUI.tsx
@@ -7,23 +7,85 @@ import {
   Alert,
   TextInput
 } from 'react-native';
+import { Dimensions } from "react-native";
+const screenWidth = Dimensions.get("window").width - 60;
+import {
+  LineChart
+} from "react-native-chart-kit";
 
+import {useState} from 'react';
 
 type InventoryProps = {
     setPage: ()=>void;
 }
 
 const ViewPredictionsUI = (props: InventoryProps) => {
+    const [histoData, setHistoData] = useState([1,2,3,4,5]);
+    const mockData = [
+     {id: 0, name: "Apple", quantity: 10},
+     {id: 1, name: "Orange", quantity: 10},
+     {id: 2, name: "Guava", quantity: 15},
+    ]
+    // Render each item in the list
+    const renderItem = ({ item }) => (
+      <View style={styles.listItem}>
+        <Text style={styles.normalText}>{item.name}</Text>
+        <Text style={styles.normalText}>{item.quantity}</Text>
+      </View>
+    );
+    const data = {
+      labels: ["January", "February", "March", "April", "May", "June"],
+      datasets: [
+        {
+          data: [20, 45, 28, 80, 99, 43],
+          color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // optional
+          strokeWidth: 2 // optional
+        }
+      ],
+      legend: ["Rainy Days"] // optional
+    };
+
+    const chartConfig = {
+      backgroundGradientFrom: COLORS.peach,
+//       backgroundGradientFromOpacity: 0,
+      backgroundGradientTo: COLORS.peach,
+//       backgroundGradientToOpacity: 0.5,
+//       color: COLORS.light,
+      color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      strokeWidth: 2, // optional, default 3
+      barPercentage: 0.5,
+      useShadowColorFromDataset: false // optional
+    };
     return(
         <View style={styles.mainBody}>
-           <View style={styles.container}>
-               <Text style={[styles.lightText, styles.headerText]}>View Predictions</Text>
-               <View>
-                  <Pressable style={[styles.mainButton, styles.marginSmaller]} onPress={() => props.setPage(0)}>
-                       <Text style={[styles.buttonText, styles.normalText]}>Return</Text>
-                   </Pressable>
-               </View>
-           </View>
+                   <View style={styles.container}>
+                          <Text style={[styles.lightText, styles.headerText]}>Analytics</Text>
+                        <Text style={[styles.lightText, styles.normalText]}>DATA ON DEMAND</Text>
+
+                         <View>
+                             <LineChart
+                               data={data}
+                               width={screenWidth}
+                               height={220}
+                               chartConfig={chartConfig}
+                             />
+
+                         </View>
+                       <Text style={[styles.lightText, styles.headerText]}>Usage Predictions</Text>
+                      <View>
+                          <View style={styles.listStyle}>
+                              <FlatList
+                                data={mockData}
+                                renderItem={renderItem}
+                                keyExtractor={(item) => item.id}
+                              />
+                          </View>
+                         <Pressable style={[styles.mainButton, styles.marginSmaller]} onPress={() => props.setPage(0)}>
+                              <Text style={[styles.buttonText, styles.normalText]}>Return</Text>
+                          </Pressable>
+                      </View>
+                   </View>
+
         </View>
    )
 }
@@ -64,12 +126,6 @@ const styles = StyleSheet.create({
        width: '100%',
        justifyContent: 'space-between'
    },
-  inputItem:{
-      flex: 1,
-      flexDirection: 'row',
-      alignItems: 'center',
-      width: '100%',
-  },
    mainButton:{
        width: '100%',
        backgroundColor: COLORS.buttonColor,
@@ -82,43 +138,16 @@ const styles = StyleSheet.create({
        textAlign: 'center'
    },
    buttonSection:{
-       marginTop: '100%',
+       marginTop: '70%',
        flexDirection: 'column',
        gap: 3
    },
-   inputName: {
-       height: 40,
-       width: "80%",
-       backgroundColor: COLORS.light,
-       borderWidth: 2,        // Border width
-       borderColor: 'black',  // Border color
-       borderRadius: 8,       // Border radius (for rounded corners)
-       padding: 10,
-   },
-    inputQty: {
-        height: 40,
-        margin: 12,
-       backgroundColor: COLORS.light,
-       borderWidth: 2,        // Border width
-       borderColor: 'black',  // Border color
-       borderRadius: 8,       // Border radius (for rounded corners)
-        padding: 10,
-    },
-   normalInput: {
-       height: 40,
-       width: "100%",
-       backgroundColor: COLORS.light,
-       borderWidth: 2,        // Border width
-       borderColor: 'black',  // Border color
-       borderRadius: 8,       // Border radius (for rounded corners)
-       padding: 10,
-   },
-   marginSmaller:{
-        marginTop: 20,
-   },
-      marginLarger:{
-           marginTop: 40,
-      }
+      marginSmaller:{
+           marginTop: 20,
+      },
+         marginLarger:{
+              marginTop: 40,
+         }
 });
 
 export default ViewPredictionsUI;
--- frontend/components/ViewSuggestionsUI.tsx
+++ frontend/components/ViewSuggestionsUI.tsx
@@ -14,14 +14,33 @@ type InventoryProps = {
 }
 
 const ViewSuggestionsUI = (props: InventoryProps) => {
+    const mockData = [
+     {id: 0, name: "Big Mac", ingredient: "1x Chicken"},
+     {id: 1, name: "Fillet o Fish", ingredient: "1x Fish"},
+     {id: 2, name: "Chicken Nugget", ingredient: "2x Chicken"},
+    ]
+    // Render each item in the list
+    const renderItem = ({ item }) => (
+      <View style={styles.suggestionItem}>
+        <Text style={styles.normalText}>{item.name}</Text>
+        <Text>{item.ingredient}</Text>
+      </View>
+    );
     return(
             <View style={styles.mainBody}>
                    <View style={styles.container}>
                        <Text style={[styles.lightText, styles.headerText]}>View Suggestions</Text>
                       <View>
-                         <Pressable style={[styles.mainButton, styles.marginSmaller]} onPress={() => props.setPage(0)}>
-                              <Text style={[styles.buttonText, styles.normalText]}>Return</Text>
-                          </Pressable>
+                      <View style={styles.listStyle}>
+                          <FlatList
+                            data={mockData}
+                            renderItem={renderItem}
+                            keyExtractor={(item) => item.id}
+                          />
+                      </View>
+                     <Pressable style={[styles.mainButton, styles.marginSmaller]} onPress={() => props.setPage(0)}>
+                          <Text style={[styles.buttonText, styles.normalText]}>Return</Text>
+                      </Pressable>
                       </View>
                    </View>
 
@@ -118,6 +137,14 @@ const styles = StyleSheet.create({
    },
       marginLarger:{
            marginTop: 40,
+      },
+      suggestionItem: {
+            marginTop: 10,
+            backgroundColor: COLORS.light,
+            borderColor: "#000000",
+            borderRadius: 7,
+            borderWidth: 1,
+            padding: 10
       }
 });
 
